19c9916d164da134ead91fec3bb84327328e95b6
Shubham Agnihotri <shubham.agnihotri1997@gmail.com>
2023-08-16 21:14:53 +0530

    zenml version 1

---
+++ run_pipeline.py
@@ -0,0 +1,5 @@
+from pipelines.training_pipeline import train_pipelines
+
+if __name__ =="__main__":
+    #run the pipeline
+    train_pipelines(data_path="D:/Github/MLOps-with-ZenML/data/olist_customers_dataset.csv")
